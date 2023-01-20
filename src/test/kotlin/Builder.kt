7258138b9978e60029722a1ea4157641eaf9026e
Mikhail Diachenko <xdiach00@stud.fit.vutbr.cz>
2023-01-20 14:03:32 +0100

    Add Builder pattern
    Builder pattern solves a problem of how to flexibly create an immutable
    object without writing many constructors

---
+++ src/test/kotlin/Builder.kt
@@ -0,0 +1,43 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Component private constructor(builder: Builder) {
+    var param1: String? = null
+    var param2: Int? = null
+    var param3: Boolean? = null
+
+    class Builder {
+        private var param1: String? = null
+        private var param2: Int? = null
+        private var param3: Boolean? = null
+
+        fun setParam1(param1: String) = apply { this.param1 = param1 }
+        fun setParam2(param2: Int) = apply { this.param2 = param2 }
+        fun setParam3(param3: Boolean) = apply { this.param3 = param3 }
+        fun build() = Component(this)
+
+        fun getParam1() = param1
+        fun getParam2() = param2
+        fun getParam3() = param3
+    }
+
+    init {
+        param1 = builder.getParam1()
+        param2 = builder.getParam2()
+        param3 = builder.getParam3()
+    }
+}
+
+class ComponentTest {
+    @Test
+    fun builderTest() {
+        val component = Component.Builder()
+            .setParam1("Value")
+            .setParam3(true)
+            .build()
+
+        Assertions.assertThat(component.param1).isEqualTo("Value")
+        Assertions.assertThat(component.param2).isNull()
+        Assertions.assertThat(component.param3).isTrue
+    }
+}
